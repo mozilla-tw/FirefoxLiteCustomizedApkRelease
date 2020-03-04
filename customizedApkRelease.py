2139c29efde9b8e1572322c6935df2265aebf0a7
daisy <pliu@mozilla.com>
2020-03-04 15:28:06 +0800

    init - hard code waiting time

---
+++ customizedApkRelease.py
@@ -0,0 +1,170 @@
+import requests
+import io ,os ,time
+
+
+RELEASE_TAG = "v2.1.10"
+PROJECT_NAME = "Firefox Lite"
+
+TARGET_ARTIFACT_TITLE = "app-focus-webkit-release-signed-aligned.apk"
+
+BUILD_TRIGGER_TOKEN = os.getenv('BUILD_TRIGGER_TOKEN') 
+BITRISE_TOKEN = os.getenv('BITRISE_TOKEN')
+BITRISE_HOSTNAME = "https://api.bitrise.io/"
+GET_APPS_URL = BITRISE_HOSTNAME + "v0.1/apps"
+
+headers = {
+    'Authorization': BITRISE_TOKEN
+    }
+
+
+CHANNEL_DIC={   "MozNext2GooglePlay" : "t5cdv3e",
+                "MozNext2APKDownload" : "qd2shkv,
+                "Github" : "8wvdqfc",
+                "SamsungStore" : "bc7t7hv",
+                "XiaomiStore" : "2bzwbyc" 
+                }
+
+def build_customized_apk(): 
+    """Trigger Bitrise to build customized apk
+    Returns:
+        dic: biuld_slugs. The returned dictionary contains build info.
+    """
+
+    START_BUILD_URL = "https://app.bitrise.io/app/{}/build/start.json".format(FXLITE_APP_SLUG)
+    
+    build_slugs={}
+
+    for channel, tracker in CHANNEL_DIC.items():
+        json = {"hook_info":
+            {"type":"bitrise",
+            "build_trigger_token":BUILD_TRIGGER_TOKEN
+            },
+                "build_params":{
+                    "commit_message": channel + "_" + RELEASE_TAG + ".apk",
+                    "workflow_id":"release_sideload_tracking","tag":RELEASE_TAG,
+                    "environments":
+                        [
+                            {"mapped_to":"ADJUST_SIDELOAD_TRACKER","value": tracker ,"is_expand":True}
+                            ]
+                            },
+                            "triggered_by":"Customized Apk Python Script"
+                    }
+
+
+        resp = requests.post(START_BUILD_URL, json=json)
+        print("customized_apk_resp.status_code", resp.status_code)
+
+        if resp.status_code == 200 or 201 :
+            build_slugs[ channel ] = { "build_slug" : resp.json()['build_slug'], "build_no" : resp.json()['build_number'] }
+            print('build is triggered, you can go to Bitrise to check and wait it finished.')
+            return build_slugs
+        else :
+            print('Unable to tigger build')
+            print(resp.text)
+
+def get_app_slug():
+    """Get project's app_slug
+    """
+
+    global FXLITE_APP_SLUG
+
+    resp = requests.get(GET_APPS_URL, headers=headers)
+    if  resp.status_code == 200:
+        all_apps=resp.json()['data']
+        for app in all_apps:
+            if app['title'] == PROJECT_NAME:
+                FXLITE_APP_SLUG = app['slug']
+                print('Retrieved app slug {} for {}'.format(FXLITE_APP_SLUG,PROJECT_NAME))
+    else:
+        print('Unable to get app slug')
+        print(resp.text)
+        
+def get_signed_apk_artifact_slug(build_slug):
+    """Get artifact_slug of signed apk
+    Args:
+        str : build_slug. A build_slug for a generated Bitrise build
+    Returns:
+        str : artifact_slug. A artifact_slug of the signed apk
+    """
+
+    GET_ARTIFACTS_URL = BITRISE_HOSTNAME + "v0.1/apps/" + FXLITE_APP_SLUG + "/builds/" + build_slug + "/artifacts"
+
+    resp = requests.get(GET_ARTIFACTS_URL, headers=headers)
+    if resp.status_code == 200 or 201:
+        all_artifacts = resp.json()['data']
+        for artifact in all_artifacts:
+            if artifact['title'] == TARGET_ARTIFACT_TITLE:
+                artifact_slug = artifact['slug']
+                print('Retrieved artifact slug {} '.format(artifact_slug))
+                return  artifact_slug
+    else:
+        print('Unable to get artifact slug')
+        print(resp.text)
+
+def get_download_url(build_slug , artifact_slug):
+    """Get customized APK Download link
+    Args:
+        str : build_slug. A build_slug for a generated Bitrise build
+        str : artifact_slug. A build_slug for a generated Bitrise build
+    Returns:
+        str : downnload_url. It is used to download apk. 
+    """
+    GET_ARTIFACT_URL = BITRISE_HOSTNAME + "v0.1/apps/" + FXLITE_APP_SLUG + "/builds/" + build_slug + "/artifacts/" + artifact_slug
+    
+    resp = requests.get(GET_ARTIFACT_URL, headers=headers)
+    if resp.status_code == 200:
+        downnload_url = resp.json()['data']['expiring_download_url']
+        print('Retrieved download url: {} '.format(downnload_url))
+        return  downnload_url
+    else:
+        print('Unable to get donwnload url')
+        print(resp.text)
+
+
+def download_customized_apk( channel , build_no , download_url):
+    """Download customized apk
+    Args:
+        str : channel. It is the channel name 
+        int : build_no. It is the build_no for the channel  
+        str : download_url. It is the download_url for the channel  
+    Returns:
+        file : apkfileName. An APK file.
+    """
+    resp = requests.get(download_url, allow_redirects= True )
+    if resp.status_code == 200:
+        apkfileName = '{}_{}_({}).apk'.format( channel ,RELEASE_TAG , build_no)
+        open(apkfileName, 'wb').write(resp.content)
+        print('Retrieved customized apk file has been written to {}'.format(apkfileName))
+    else:
+        print('Unable to download customized apk')
+        print(resp.text)
+
+
+def main():
+    get_app_slug()
+    build_slugs = build_customized_apk()
+    print('Show channel build_slug information {}'.format(build_slugs))
+
+    """
+    Need to wait around 15 min to get generated build artifact . The hard code waiting time will improve in the future.
+    """
+    time.sleep(15 * 60)
+    
+    """
+    itertate each channel build-slug info to download apks
+    """
+    for channel, slug_info in build_slugs.items():
+        build_slug , build_no = slug_info["build_slug"], slug_info["build_no"]
+        artifact_slug = get_signed_apk_artifact_slug( build_slug)
+        download_url = get_download_url( build_slug, artifact_slug)
+        download_customized_apk( channel, build_no, download_url)
+
+if __name__ == '__main__':
+    main()
+    
+    """
+    Testcase_1: 
+        global FXLITE_APP_SLUG 
+        FXLITE_APP_SLUG = "2bee753c3b6709ca"
+        build_slugs = {'MozNext2GooglePlay': {'build_slug': '1b41a8e4312baf2c', 'build_no': 18844}}
+    """
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Firefox Lite Customized Apk Release Handy Tool
+
+ This script can be used to trigger [Bitrise](https://app.bitrise.io/app/2bee753c3b6709ca) to get customized build and then uplaod to Google Drive
+
+## Prerequisition
+1. export "BITRISE_TOKEN" as enviromnent variable. To get BITRISE_TOKEN refer [Bitrise acquiring a Personal Access Token ](https://devcenter.bitrise.io/jp/api/authentication/)
+2. export "BITRISE_TRIGGER_TOKEN" as enviromnent variable. To get BITRISE_TRIGGER_TOKEN please go to [Fx LiteBitrise]https://app.bitrise.io/app/2bee753c3b6709ca#() > Start a Build > Advanced
+
+
+## Installation
+
+Use the package manager [pip3](https://pip.pypa.io/en/stable/) to install Remote Config Script.
+
+```bash
+pip3 install requirements.txt
+```
+
+## Usage
+ 
+Please use the following commands:
+
+```bash
+python3 customizedApkRelease.py 
+
+```
+## Authors
+
+* **[Daisy Liu](https://github.com/Daisy-pliu)** 
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
